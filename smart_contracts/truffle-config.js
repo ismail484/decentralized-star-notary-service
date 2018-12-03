080f5dfbff0ef3eacfa5c151461e12653f953a14
ismailm <ismailm@oclc.org>
2018-12-03 21:22:15 +0100

    add ontracts using truffle

---
+++ smart_contracts/truffle-config.js
@@ -0,0 +1,36 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() { 
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+
+const mnemonic = "illegal fuel snake anger woman weapon sibling glow embrace tired prosper cousin"
+const infura = "https://rinkeby.infura.io/v3/8366432e46054a6c89a5cdee8f6df725"
+
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+module.exports = {
+networks: {
+  development: {
+   host: "127.0.0.1",
+   port: 9545,
+   network_id: "*" // Match any network id
+ },
+ rinkeby: {
+  provider: function() {
+ return new HDWalletProvider(mnemonic,infura )
+     },
+      network_id: '4',
+      gas: 4500000,
+      gasPrice: 10000000000,
+    }
+   }
+ };
